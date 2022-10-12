3544665daca37e978dcf7d53d67ebdacfde003d6
david-alonzo <78769397+alonzo-david@users.noreply.github.com>
2022-10-11 21:07:22 -0600

    db config

--- build/config/config.js
+++ build/config/config.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.DATA_SOURCES = void 0;
 exports.DATA_SOURCES = {
     MySQL: {
-        HOST: "buwff2akn4ax6rfaudnk-mysql.services.clever-cloud.com",
-        USER: "uwbvlj8npfbhzamp",
-        PASSWORD: "HHHkDNE2N6dS1WGx2iri",
+        HOST: "ls-1b6944a609e1c53dd2129e66fca600bd4b8e16a0.cwe4masmqftx.us-east-2.rds.amazonaws.com",
+        USER: "tds_pds",
+        PASSWORD: "tds_pds_2022",
         PORT: '3306',
-        DATABASE: "buwff2akn4ax6rfaudnk"
+        DATABASE: "dbmaster"
     },
 };
