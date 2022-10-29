d55162b6324d70213beb58a502a281abd8fcf5a5
david-alonzo <78769397+alonzo-david@users.noreply.github.com>
2022-10-28 20:57:06 -0600

    Update config.js

--- build/config/config.js
+++ build/config/config.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.DATA_SOURCES = void 0;
 exports.DATA_SOURCES = {
     MySQL: {
-        HOST: "ls-1b6944a609e1c53dd2129e66fca600bd4b8e16a0.cwe4masmqftx.us-east-2.rds.amazonaws.com",
-        USER: "tds_pds",
-        PASSWORD: "tds_pds_2022",
+        HOST: "xxxxx",
+        USER: "xx",
+        PASSWORD: "xxxxxx",
         PORT: '3306',
-        DATABASE: "dbmaster"
+        DATABASE: "xxxxxxx"
     },
 };
